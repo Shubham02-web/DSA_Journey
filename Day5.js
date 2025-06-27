b857ff1121ed80862d6e136eb175ad04dd9a56b0
Shubham Vishwakarma <vi453770@gmail.com>
2025-06-28 00:06:57 +0530

    Type Corression and type casting

--- Day5.js
+++ Day5.js
@@ -1,3 +1,24 @@
 // Start The Play List DSA with JAVASCRIPT FROM SHREYANSH CODING SCHOOLS
+
+// Type Casting
 const age = Number(prompt("What is your age ? "));
 console.log(age);
+
+// Type Corresion
+console.log(2 + 1 + "Hey i am 3");
+console.log("hey i am 21 : ;" + 2 + 1);
+
+//  Hey i am concatination
+console.log("2" + 2);
+
+// Addition
+
+console.log(2 + 2);
+
+// Type Corression by default from JS Complier
+// Substriction
+console.log("2" - 1); // output :- 1
+
+console.log("1" * 1); // output :- 1 do actually multiplication
+
+console.log("1" / 1); // output:-  1 do division
