3057a4e4423c69779e5f88ca705ee4bde360a944
Shubham Vishwakarma <vi453770@gmail.com>
2025-06-29 00:17:48 +0530

    Have Done Learning of Basics in JavaScript

--- Day7.js
+++ Day7.js
@@ -15,8 +15,51 @@
 
 // console.log(a % 100);
 
-let i = 11;
-// let j = i++;
+// let i = 11;
+// i = i++;
 // i = i++ + ++i;
 // i = ++i;
-console.log(i);
+// i = ++(++i);
+
+// i = i--;
+// i = --i;
+
+// i = i-- - --i;
+
+// console.log(i);
+
+// Area of Rectangle
+
+// let length = 10;
+// let width = 5;
+// console.log("Area of Rectangle = ", length * width);
+
+// Genrate OTP
+
+// function genrateOtp(n) {
+//     let otp = "";
+//     while (n > 0) {
+//         otp += Math.round(Math.random() * 10 + 1);
+//         n--;
+//     }
+//     return otp;
+// }
+
+// console.log(genrateOtp(6));
+
+// Area of triangle using heron's problem
+
+// let a = 5;
+// let b = 7;
+// let c = 8;
+
+// let s = (a + b + c) / 2;
+
+// let area = Math.round(Math.sqrt(s * (s - a) * (s - b) * (s - c)));
+// console.log(area);
+
+//   Circumference Of area
+
+let r = 7;
+let cir = (2 * Math.PI * r).toFixed(2);
+console.log(cir);
