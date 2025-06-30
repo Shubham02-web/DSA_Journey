cd5b0df417cb660ba057efdc64f77f45362e183c
Shubham Vishwakarma <vi453770@gmail.com>
2025-06-30 23:23:20 +0530

    manage shop discount

--- Day8.js
+++ Day8.js
@@ -1,16 +1,43 @@
 // To check valid voter
 
-const age = Number(prompt("Please Enter Your Age"));
-if (isNaN(age)) {
-  alert("Enter Number");
-  console.log("Please enter number its not a number");
-} else if (Math.floor(age <= 0)) {
-  alert("Please Enter a positive number");
-  console.log("Please Enter a positive number");
-} else if (Math.floor(age <= 18)) {
-  alert("You are under 18 you are not eligible for vote now ");
-  console.log("You are under 18");
-} else {
-  alert("Congratulation you are eligble for voting");
-  console.log("Congratulation you are eligble for voting");
-}
+// const age = Number(prompt("Please Enter Your Age"));
+// if (isNaN(age)) {
+//   alert("Enter Number");
+//   console.log("Please enter number its not a number");
+// } else if (Math.floor(age <= 0)) {
+//   alert("Please Enter a positive number");
+//   console.log("Please Enter a positive number");
+// } else if (Math.floor(age <= 18)) {
+//   alert("You are under 18 you are not eligible for vote now ");
+//   console.log("You are under 18");
+// } else {
+//   alert("Congratulation you are eligble for voting");
+//   console.log("Congratulation you are eligble for voting");
+// }
+
+// Manage Shop Discount
+
+// let amount = Number(prompt("total amount  = ? "));
+// let Discount;
+// if (isNaN(amount)) {
+//   alert("Enter valid number ");
+//   console.log("Please Enter a valid number ");
+// } else if (amount >= 0 && amount <= 5000) {
+//   console.log("Total Amount : " + amount);
+//   Discount = Math.floor((amount * 5) / 100);
+//   console.log("Total Discount :  " + Discount);
+//   amount = amount - Discount;
+//   console.log("Total Paybale Amount : " + amount);
+// } else if (amount > 5000 && amount <= 8000) {
+//   console.log("Total Amount : " + amount);
+//   Discount = Math.floor((amount * 10) / 100);
+//   console.log("Total Discount :  " + Discount);
+//   amount = amount - Discount;
+//   console.log("Total Paybale Amount : " + amount);
+// } else if (amount > 8000) {
+//   console.log("Total Amount : " + amount);
+//   Discount = Math.floor((amount * 20) / 100);
+//   console.log("Total Discount :  " + Discount);
+//   amount = amount - Discount;
+//   console.log("Total Paybale Amount : " + amount);
+// }
