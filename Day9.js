45ac920a82f24a7956088011704d1b97705c0ea5
Shubham Vishwakarma <vi453770@gmail.com>
2025-07-01 22:58:46 +0530

    N natural number and vice versa

--- Day9.js
+++ Day9.js
@@ -1,8 +1,17 @@
 //  Print n hellow words
 
-function nhellowWord(n) {
-  for (let i = 0; i < n; i++) {
-    console.log("Hello World" + "  " + (i + 1));
+// function nhellowWord(n) {
+//   for (let i = 0; i < n; i++) {
+//     console.log("Hello World" + "  " + (i + 1));
+//   }
+// }
+// nhellowWord(5);
+
+function naturalNumer(n) {
+  let value = [];
+  for (let i = n; i > 0; i--) {
+    value.push(i);
   }
+  return value;
 }
-nhellowWord(5);
+console.log(...naturalNumer(10));
