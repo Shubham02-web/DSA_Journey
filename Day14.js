49ae9a452310839a4cf3d9b4337e9532740d6829
Shubham Vishwakarma <vi453770@gmail.com>
2025-07-14 21:48:37 +0530

    find missing Number using nestead loops

--- Day14.js
+++ Day14.js
@@ -3,12 +3,24 @@
 // find missing Number Among 0 to n+1
 
 // using reduce method
-function findMissing(nums) {
-    let n = nums.length;
-    let expected = (n * (n + 1)) / 2;
-    let actual = nums.reduce((a, b) => a + b, 0);
-    return expected - actual;
-}
+// function findMissing(nums) {
+//     let n = nums.length;
+//     let expected = (n * (n + 1)) / 2;
+//     let actual = nums.reduce((a, b) => a + b, 0);
+//     return expected - actual;
+// }
 
-let arr = [2, 3, 0, 4, 1, 5];
+// using nestead loop
+function findMissing(nums) {
+    for (let i = 0; i < nums.length + 1; i++) {
+        let flag = 1;
+        for (let j = 0; j < nums.length + 1; j++) {
+            if (i === nums[j]) {
+                flag = 0;
+            }
+        }
+        if (flag) return i;
+    }
+}
+let arr = [2, 3, 6, 4, 0, 5];
 console.log(findMissing(arr));
