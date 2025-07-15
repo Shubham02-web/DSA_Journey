849128c6659f4640e754686ae9f1ba8710ddf479
Shubham Vishwakarma <vi453770@gmail.com>
2025-07-15 22:52:37 +0530

    revision of findDuplicate  using neseted loops and frequenty

--- Day15.js
+++ Day15.js
@@ -1,16 +1,46 @@
 // Find duplicate numbers in an array containing multiple duplicates.
 
+// function findDuplicate(nums) {
+//     let temp = [];
+//     let seen = new Set();
+//     for (let num of nums) {
+//         if (seen.has(num)) {
+//             temp[temp.length] = num;
+//         } else {
+//             seen.add(num);
+//         }
+//     }
+//     return temp;
+// }
+
 function findDuplicate(nums) {
-    let temp = [];
-    let seen = new Set();
-    for (let num of nums) {
-        if (seen.has(num)) {
-            temp[temp.length] = num;
+    let frequencyArr = [];
+    let result = [];
+    for (let i = 0; i < nums.length; i++) {
+        let val = nums[i];
+        if (frequencyArr[val] === undefined) {
+            frequencyArr[val] = 1;
         } else {
-            seen.add(num);
+            frequencyArr[val]++;
         }
     }
-    return temp;
+
+    for (let j = 0; j < frequencyArr.length; j++) {
+        if (frequencyArr[j] > 1) {
+            let isAdded = true;
+            for (let k = 0; k < result.length + 1; k++) {
+                if (result[k] === j) {
+                    isAdded = false;
+                    break;
+                }
+            }
+            if (isAdded) {
+                result[result.length] = j;
+            }
+        }
+    }
+
+    return result;
 }
 let array = [3, 5, 7, 2, 3, 2];
 console.log(findDuplicate(array));
