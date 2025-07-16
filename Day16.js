59316c1554d5752b9c27436e0c90570621600827
Shubham Vishwakarma <vi453770@gmail.com>
2025-07-16 22:14:22 +0530

    reverse array

--- Day16.js
+++ Day16.js
@@ -12,22 +12,39 @@
 //     return temp;
 // }
 
-function findDuplicate(nums) {
-    let slow = nums[0];
-    let fast = nums[0];
-    do {
-        slow = nums[slow];
-        fast = nums[nums[fast]];
-    } while (slow != fast);
+// function findDuplicate(nums) {
+//     let slow = nums[0];
+//     let fast = nums[0];
+//     do {
+//         slow = nums[slow];
+//         fast = nums[nums[fast]];
+//     } while (slow != fast);
 
-    slow = nums[0];
-    while (slow != fast) {
-        slow = nums[slow];
-        fast = nums[fast];
+//     slow = nums[0];
+//     while (slow != fast) {
+//         slow = nums[slow];
+//         fast = nums[fast];
+//     }
+//     return slow;
+// }
+
+// let arr = [1, 2, 3, 3, 4, 5, 6, 7];
+
+// console.log(findDuplicate(arr));
+
+function reverseArray(nums) {
+    let left = 0;
+    let right = nums.length - 1;
+    let temp = 0;
+    while (left < right) {
+        temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+        left++;
+        right--;
     }
-    return slow;
+    return nums;
 }
 
-let arr = [1, 2, 3, 3, 4, 5, 6, 7];
-
-console.log(findDuplicate(arr));
+let arr = [1, 2, 3, 4, 5];
+console.log(reverseArray(arr));
