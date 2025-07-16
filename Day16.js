aeca19b1789838120ccbec832f76cf0fde67f663
Shubham Vishwakarma <vi453770@gmail.com>
2025-07-16 22:27:44 +0530

    find intersection all the matching leetcode 350

--- Day16.js
+++ Day16.js
@@ -49,18 +49,40 @@
 // let arr = [1, 2, 3, 4, 5];
 // console.log(reverseArray(arr));
 
-function intersection(nums1, nums2) {
-    for (let num1 of nums1) {
-        for (let num2 of nums2) {
-            if (num1 === num2) {
-                return num1;
-                break;
-            }
+// function intersection(nums1, nums2) {
+//     for (let num1 of nums1) {
+//         for (let num2 of nums2) {
+//             if (num1 === num2) {
+//                 return num1;
+//                 break;
+//             }
+//         }
+//     }
+// }
+
+function findIntersection(nums1, nums2) {
+    nums1.sort((a, b) => a - b);
+    nums2.sort((a, b) => a - b);
+
+    let result = [];
+    let i = 0,
+        j = 0;
+    while (i < nums1.length && j < nums2.length) {
+        if (nums1[i] === nums2[j]) {
+            result[result.length] = nums1[i];
+            i++;
+            j++;
+        } else if (nums1[i] < nums2[j]) {
+            i++;
+        } else {
+            j++;
         }
     }
+    return result;
 }
 
-let nums1 = [4, 9, 5];
+let nums1 = [4, 9, 5, 4];
 let nums2 = [9, 4, 9, 8, 4];
 
-console.log(intersection(nums1, nums2));
+// console.log(intersection(nums1, nums2));
+console.log(findIntersection(nums1, nums2));
