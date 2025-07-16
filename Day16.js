8259626f47ca43413a160ac859ae500ef85c1ea0
Shubham Vishwakarma <vi453770@gmail.com>
2025-07-17 00:13:43 +0530

    find   Find the maximum product of two integers in an array.

--- Day16.js
+++ Day16.js
@@ -60,29 +60,51 @@
 //     }
 // }
 
-function findIntersection(nums1, nums2) {
-    nums1.sort((a, b) => a - b);
-    nums2.sort((a, b) => a - b);
+// function findIntersection(nums1, nums2) {
+//     nums1.sort((a, b) => a - b);
+//     nums2.sort((a, b) => a - b);
 
-    let result = [];
-    let i = 0,
-        j = 0;
-    while (i < nums1.length && j < nums2.length) {
-        if (nums1[i] === nums2[j]) {
-            result[result.length] = nums1[i];
-            i++;
-            j++;
-        } else if (nums1[i] < nums2[j]) {
-            i++;
-        } else {
-            j++;
-        }
-    }
-    return result;
-}
+//     let result = [];
+//     let i = 0,
+//         j = 0;
+//     while (i < nums1.length && j < nums2.length) {
+//         if (nums1[i] === nums2[j]) {
+//             result[result.length] = nums1[i];
+//             i++;
+//             j++;
+//         } else if (nums1[i] < nums2[j]) {
+//             i++;
+//         } else {
+//             j++;
+//         }
+//     }
+//     return result;
+// }
 
-let nums1 = [4, 9, 5, 4];
-let nums2 = [9, 4, 9, 8, 4];
+// let nums1 = [4, 9, 5, 4];
+// let nums2 = [9, 4, 9, 8, 4];
 
 // console.log(intersection(nums1, nums2));
-console.log(findIntersection(nums1, nums2));
+// console.log(findIntersection(nums1, nums2));
+
+// new question
+// 9.	Find the maximum product of two integers in an array.
+
+function maxProduct(nums) {
+    let max1 = nums[0];
+    let max2 = nums[0];
+    for (let num of nums) {
+        if (max1 < num) {
+            max2 = max1;
+            max1 = num;
+        } else if (max2 < num) {
+            max2 = num;
+        }
+    }
+
+    return (max1 - 1) * (max2 - 1);
+}
+
+// let nums = [1, 5, 4, 5];
+let nums = [3, 7];
+console.log(maxProduct(nums));
