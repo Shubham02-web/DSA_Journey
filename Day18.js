7e4229bf65b29bd6f26fefb46970024760333f99
Shubham Vishwakarma <vi453770@gmail.com>
2025-07-18 22:37:37 +0530

    Revsion done for rotate array by k using left right approach

--- Day18.js
+++ Day18.js
@@ -19,16 +19,32 @@
 
 // console.log(subarr([6, 3, 6], 6));
 
+// function rotateArr(nums, k) {
+//     for (let i = 0; i < k; i++) {
+//         let temp = nums[nums.length - 1];
+//         for (let j = nums.length - 2; j >= 0; j--) {
+//             nums[j + 1] = [nums[j + 1] + nums[j]] - nums[j + 1];
+//         }
+//         nums[0] = temp;
+//     }
+//     return nums;
+// }
+
 function rotateArr(nums, k) {
-    for (let i = 0; i < k; i++) {
-        let temp = nums[nums.length - 1];
-        for (let j = nums.length - 2; j >= 0; j--) {
-            nums[j + 1] = [nums[j + 1] + nums[j]] - nums[j + 1];
-        }
-        nums[0] = temp;
-    }
+    k = k % nums.length;
+    reverse(nums, 0, nums.length - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, nums.length - 1);
     return nums;
 }
 
+function reverse(arr, start, end) {
+    while (start <= end) {
+        [arr[start], arr[end]] = [arr[end], arr[start]];
+        start++;
+        end--;
+    }
+}
+
 let arr = [1, 2, 3, 4, 5, 6, 7];
-console.log(rotateArr(arr, 3));
+console.log(rotateArr(arr, 4));
