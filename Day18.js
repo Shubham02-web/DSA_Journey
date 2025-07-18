f870b06ff992e1d045b3c089feb2fd354c5a669b
Shubham Vishwakarma <vi453770@gmail.com>
2025-07-18 22:26:05 +0530

    rotate array from kth place using nestead loops

--- Day18.js
+++ Day18.js
@@ -1,20 +1,34 @@
 // revision
 
 // find the sub array of given inetger numbers
-function subarr(nums, k) {
-    let start = 0;
-    let subSum = 0;
-    for (let end = 0; end <= nums.length; end++) {
-        subSum = subSum + nums[end];
-        if (subSum > k) {
-            subSum = subSum - nums[start];
-            start++;
+// function subarr(nums, k) {
+//     let start = 0;
+//     let subSum = 0;
+//     for (let end = 0; end <= nums.length; end++) {
+//         subSum = subSum + nums[end];
+//         if (subSum > k) {
+//             subSum = subSum - nums[start];
+//             start++;
+//         }
+//         if (subSum == k) {
+//             return nums.slice(start, end + 1);
+//         }
+//     }
+//     return [];
+// }
+
+// console.log(subarr([6, 3, 6], 6));
+
+function rotateArr(nums, k) {
+    for (let i = 0; i < k; i++) {
+        let temp = nums[nums.length - 1];
+        for (let j = nums.length - 2; j >= 0; j--) {
+            nums[j + 1] = [nums[j + 1] + nums[j]] - nums[j + 1];
         }
-        if (subSum == k) {
-            return nums.slice(start, end + 1);
-        }
+        nums[0] = temp;
     }
-    return [];
+    return nums;
 }
 
-console.log(subarr([6, 3, 6], 6));
+let arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(rotateArr(arr, 3));
